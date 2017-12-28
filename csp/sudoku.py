66aa66971a6e525b9f64100976705a99e94f88a4
Aryella Lacerda <aryella.lacerda@gmail.com>
2017-12-28 15:00:04 -0300

    Adicionar classe Quadrante, e implementar alguns métodos do BKTRK.

--- csp/sudoku.py
+++ csp/sudoku.py
@@ -1,17 +1,35 @@
-# TODO: Implementar buscarVariavelNaoAtribuida()
-# TODO: Implementar estadoFinal(atual)
-# TODO: Implementar ordenarValoresDoDominio(var)
-# TODO: Implementar atribuir(var, val, atual)
 # TODO: Implementar propagar(var, val):
 # TODO: Implementar removerAtribuicao(var, val, atual)
 # TODO: Implementar removerPropagacao(var, val)
 
-class Quadrado:
-    def __init__(self, n = None):
-        '''Recebe o n do quadrado, caso seja informado. Por segurança, n = None por padrão.'''
+from collections import namedtuple
+Coordenada = namedtuple('Coordenada', 'x y')
+
+class Quadrante:
+    def __init__(self, n, linhas, colunas):
+        #Os indices das 3 linhas e das 3 colunas que compõem o quadrante.
+        #Para faciliar iteração pelos quadrados daquele quadrante.
         self.n = n
-        if self.n is None: self.dominio = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        else:              self.dominio = [n]
+        self.linhas = linhas
+        self.colunas = colunas
+
+class Quadrado:
+    def __init__(self, coordenada, quadrante, n = None):
+        '''
+        Recebe o n do quadrado, caso seja informado. Por segurança, n = None por padrão.
+        Recebe uma Coordenada representado as coordenada do quadrado, e um Quadrante correspondente.
+        '''
+        self.n = n
+        self.coordenada = coordenada
+        self.quadrante = quadrante
+
+        # Operações feitas sobre o domínio: adicionar, remover, contém/não contém
+        # Dominio poderia ser representado por uma lista ou por um set.
+        # Lista: append(1), remove O(n), in/not in O(n)
+        # Set: add O(1), remove O(1), in/not in O(1)
+
+        if self.n is None: self.dominio = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        else:              self.dominio = {n}
 
     def tamDominio(self):
         return len(self.dominio)
@@ -22,31 +40,109 @@ class Quadrado:
 class Sudoku:
     def __init__(self, matriz):
         '''Recebe uma matriz 9x9 do tipo inteiro para processamento.'''
+        self._nVarNaoAtribuidas = 0;
+        self._quadrantes = self._initQuadrantes()
         self.sudoku = self._processarMatriz(matriz)
+
+    def _initQuadrantes():
+        quadrantes = []
+        i = 0
+        for linhas in [[0, 1, 2], [3, 4, 5], [6, 7, 8]]:
+            for colunas in [[0, 1, 2], [3, 4, 5], [6, 7, 8]]:
+                quadrantes.append( Quadrante(i, linhas, colunas) )
+                i += 1
+        return quadrantes
 
     # A título de curiosidade, poderiamos ter feito isso:
     # return '\n'.join([' '.join([str(n) for n in lin]) for lin in self.sudoku])
+    # Seria mais rápido, menos legível, e sem formatação
+    #O(n*m)
     def __repr__(self):
         matrizStr = []
         for i, linha in enumerate(self.sudoku):
             linhaStr = []
             for j, quadrado in enumerate(linha):
                 if j % 3 == 0 and j != 0:   linhaStr.append('|')
-                if quadrado.n is not None:  linhaStr.append(str(quadrado.n))
+                if quadrado.n is not None:  linhaStr.append(str(quadrado))
                 else:                       linhaStr.append('x')
             if i % 3 == 0 and i != 0:
                 matrizStr.append('---------------------')
             matrizStr.append(' '.join(linhaStr))
         return '\n'.join(matrizStr)
 
+    #O(n*m)
     def _processarMatriz(self, matriz):
         '''Recebe uma matriz 9x9 do tipo inteiro para processamento.'''
         tabuleiroProcessado = []
-        for linha in matriz:
+        for i, linha in enumerate(matriz):
             linhaProcessada = []
-            for n in linha:
-                if n != 0: elemento = Quadrado(n)
-                else:      elemento = Quadrado(None)
+            for j, num in enumerate(linha):
+
+                #Configurando cada Quadrado individualmente
+                coord = Coordenada(i,j)
+                quad = self._defQuadrante(coord)
+                if num != 0:
+                    elemento = Quadrado(coord, quad, num)
+                else:
+                    elemento = Quadrado(coord, quad, None)
+                    self._nVarNaoAtribuidas += 1
+
                 linhaProcessada.append(elemento)
             tabuleiroProcessado.append(linhaProcessada)
         return tabuleiroProcessado
+
+    def _defQuadrante(self, coordenada):
+        '''
+        Recebe uma Coordenada, retorna um dos 9 Quadrantes do tabuleiro.
+        '''
+        x, y = coordenada
+        if x >= 0 and x < 3:
+            if y >= 0 and y < 3: return self._quadrantes[0]
+            if y < 6: return self._quadrantes[1]
+            if y < 9: return self._quadrantes[2]
+        if x < 6:
+            if y >= 0 and y < 3: return self._quadrantes[3]
+            if y < 6: return self._quadrantes[4]
+            if y < 9: return self._quadrantes[5]
+        if x < 9:
+            if y >= 0 and y < 3: return self._quadrantes[6]
+            if y < 6: return self._quadrantes[7]
+            if y < 9: return self._quadrantes[8]
+
+    #O(n*m)
+    def buscarVariavelNaoAtribuida(self):
+        for linha in self.sudoku:
+            for quadrado in linha:
+                if quadrado.n is None:
+                    return quadrado
+        return None
+
+    #O(1)
+    def estadoFinal(self):
+        return self._nVarNaoAtribuidas == 0
+
+    def ordenarValoresDoDominio(self, var):
+        '''
+        Recebe uma variável do tipo Quadrado.
+        Retorna uma lista com seu domínio, ordenado.
+        '''
+        #Uma heuristica é escolher o valor que causa menos restrições nas demais variáveis. Inicialmente sem ordenação, porque isso parece complicado de implementar.
+        #LCV - Least Constraining Value
+        return var.dominio
+
+    def atribuir(self, var, val):
+        var.n = val
+
+    def propagar(self, var, val):
+        x, y = var.coordenada
+        matriz = self.sudoku # Readability
+
+        #Procurar val no dominio das variáveis não atribuídas daquela linha e remover
+        linha = [quad for quad in matriz[x] if (quad.n is None and val in quad.dominio)]
+        for quad in linha:
+            quad.dominio.remove(val)
+
+        #Procurar val no dominio das variáveis não atribuídas daquela coluna e remover
+        coluna = [matriz[i][j] for i in range(9) if (matriz[i][j].n is None and val in matriz[i][j].dominio)]
+        for quad in coluna:
+            quad.dominio.remove(val)
