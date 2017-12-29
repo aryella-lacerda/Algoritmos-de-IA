1b17055a898740811147a367878b0a6f15e45133
RAUL OLIVEIRA DE ANDRADE <raul.andrade@dcomp.ufs.br>
2017-12-29 14:34:29 -0300

    add show

--- csp/csp.py
+++ csp/csp.py
@@ -1,6 +1,20 @@
 class csp(object):
 
     def __init__(self, domains,arcs, constraint):
-        self.domains = domains # [1-9]
+        self.domains = domains # key -> [1-9]
         self.arcs = arcs
-        self.constraint = constraint 
+        self.constraint = constraint
+
+    def show_domains(self):
+        print('Dominio:')
+        for key in self.domains:
+            print('%s :' %(key)),
+            print(self.domains[key])
+
+    def show_arcs(self):
+        print('Vizinho:')
+        for key in self.arcs:
+            print('%s :' %(key)),
+            print(' '.join(self.arcs[key]))
+
+    
