5c241314d97cfae431576a5d16b32eee7e01737f
RAUL OLIVEIRA DE ANDRADE <raul.andrade@dcomp.ufs.br>
2017-12-29 13:28:20 -0300

    problem CSP

---
+++ csp/csp.py
@@ -0,0 +1,6 @@
+class csp(object):
+
+    def __init__(self, domains,arcs, constraint):
+        self.domains = domains # [1-9]
+        self.arcs = arcs
+        self.constraint = constraint 
---
+++ csp/sudoku.py
@@ -0,0 +1,3 @@
+import csp
+import copy
+import queue
