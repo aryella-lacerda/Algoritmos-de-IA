b8209134931444f3f8360ae7a9311ce492458723
RAUL OLIVEIRA DE ANDRADE <raul.andrade@dcomp.ufs.br>
2018-01-07 01:13:35 -0300

    adicionando funcoes de conflitos

---
+++ csp/mainQueens.py
@@ -0,0 +1,49 @@
+from queens import Queens
+
+#teste antes de colocar na classe de queens.py
+def attacks(column,i,j):
+    if int(column[i]) == int(column[j]): return True
+    if abs(i-j) == abs(int(column[i]) - int(column[j])): return True
+    return False
+
+#teste antes de colocar na classe de queens.py
+def checkQueen(line):
+    s = 0
+    for i in line:
+        s += int(i)
+    return s
+
+#teste antes de colocar na classe de queens.py
+def constrainsViolated(Q):
+    '''
+        fazendo o J receber I consigo fazer a compraçao das colunas em O(n)
+        o if do final é por que nunca era feita a comparacao do I = 0 e J = MAXSIZE
+    '''
+    for i in range(len(Q)):
+        for j in range(i+1,len(Q)):
+            if(attacks(Q[i],i,j)): #i -> coluna, j ->linha
+                print('attacks')
+        if i == len(Q)-1:
+            if attacks(Q[0],i,len(Q)-1):
+
+                print('attacks')
+def readFile():
+
+    with open('queens.txt') as infile:
+        # Read and Format Game Board
+        dimensions = infile.readline().split(' ')
+        nLines,nColumns = int(dimensions[0]),int(dimensions[1])
+        lines = infile.readlines()
+        gameBoard = []
+        for line in lines:
+            line = line.rstrip()
+            if line != '':
+                gameBoard.append(line.split(' '))
+
+        #for line in gameBoard:
+        #    print(checkQueen(line))
+
+        print(gameBoard)
+        constrainsViolated(gameBoard)
+
+readFile()
--- csp/queens.py
+++ csp/queens.py
@@ -7,6 +7,16 @@ class Element:
         self.index = index
         self.conflicts = 0
 
+    def __str__(self):
+        return f'{self.element}'
+
+    def __iter__(self):
+        for i in range(1):
+            yield self
+
+    __repr__ = __str__
+
+
 class Queens:
 
     def __init__(self, lines, columns, gameBoard):
@@ -15,8 +25,20 @@ class Queens:
         self._gameBoard = gameBoard
 
         #Public
-        self.queen = None
+        self.conflictsLine = None
+        self.conflictsColumns = None
 
+        #Procedures
+        self._processGameBoard()
+
+    def __str__(self):
+        totalLines = []
+        for line in self._gameBoard:
+            currentLine = []
+            for element in line:
+                currentLine.append(str(element.element))
+            totalLines.append(' '.join(currentLine))
+        return '\n'.join(totalLines)
 
     def _processGameBoard(self):
         processedGame = []
@@ -25,8 +47,37 @@ class Queens:
             for m, element in enumerate(line):
                 currentElement = Element(element,Index(n,m))
                 currentLine.append(currentElement)
-                if elemen == '1': self.queen = currentElement #encontrar uma rainha
+                #if element == '1': self.queen = currentElement #encontrar uma rainha
 
                 #talvez precise colocar uma funcao pra saber os conflitos aqui
-
             processedGame.append(currentLine)
+            self._gameBoard = processedGame
+
+    '''
+        talvez facilite tratar colunas como linha e linhas como coluna
+        como em python se faz lista de Lista e.g
+        [[....],[....],[....],[....]]
+        deixando mais visual
+        [
+            [....],
+            [....],
+            [....],
+            [....]
+        ]
+        cada lista dentro da lista seria uma coluna, pois nesse problema
+        nao pode por duas rainhas na mesma linhas
+        dai ja limita muito o espaço de busca
+    '''
+    def horizontalCheck(self, line): #line é uma lista
+        return sum(line) #se for  > 1 tem alguma rainha na horizontal
+
+    '''talvez essa parte aqui nem precise
+    def verticalCheck(self,column): #line é uma lista
+        return sum(column) # se for > 1 tem alguma rainha na vertica
+    '''
+    def attacks(column, i ,j):
+        #quando coloca uma rainha rainha em uma linha ela passa a ser ignorada
+        #por isso verificar coluna
+        if column[i] == column[j]: return True
+        if abs(i-j) == abs(column[i] - column[j]): return True
+        return False
